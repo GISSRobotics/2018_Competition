766767af97a68d324e479896bcf2e5b07ba99785
SD64\robotics15 <seb4756@sd64.bc.ca>
2018-01-14 15:02:23 -0800

    nothing has changed

--- src/org/usfirst/frc6406/commands/clawclose.java
+++ src/org/usfirst/frc6406/commands/clawclose.java
@@ -39,18 +39,22 @@ public class clawclose extends Command {
     // Called just before this Command runs the first time
     @Override
     protected void initialize() {
+    	
+    	
+    	int x = 1 / 0;
     	Robot.claw.close();
     }
 
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+    
     }
 
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-        return isTimedOut();
+        return false;
     }
 
     // Called once after isFinished returns true
--- src/org/usfirst/frc6406/commands/clawopen.java
+++ src/org/usfirst/frc6406/commands/clawopen.java
@@ -39,18 +39,20 @@ public class clawopen extends Command {
     // Called just before this Command runs the first time
     @Override
     protected void initialize() {
+    	int x = 1 / 0;
     	Robot.claw.open();
     }
 
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-        return isTimedOut();
+        return false;
     }
 
     // Called once after isFinished returns true
