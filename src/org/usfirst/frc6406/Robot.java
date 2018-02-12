a8fc0c74e5d734744da9a5fea24e9c600c4bf1c8
Cohen <Cohen@192.168.7.47>
2018-02-12 13:12:10 -0800

    working joy

--- src/org/usfirst/frc6406/Robot.java
+++ src/org/usfirst/frc6406/Robot.java
@@ -57,6 +57,7 @@ public class Robot extends TimedRobot {
        // claw = new claw();
         //wrist = new wrist();
         drive = new Drive();
+        
        
         //sensors = new Sensors();
         //climber = new Climber();
@@ -109,6 +110,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopInit() {
+    	new JoystickDrive().start();
         // This makes sure that the autonomous stops running when
         // teleop starts running. If you want the autonomous to
         // continue until interrupted by another command, remove
